9371f228d8e5d39a6fb600031b68e2149545c6d1
Julius Palcong <ppdo.kmdr.julius@gmail.com>
2026-09-15 11:27:04 +0800

    fixed sub-items for sarai and i hub operations

--- src/App.jsx
+++ src/App.jsx
@@ -183,8 +183,8 @@ const data2027 = [
       { name: "Press Releases / Planning Insights", activities: [
         { name: "Press releases published", months: [3,6,9,12] },
       ]},
-      { name: "Deployment of KMDR Staff for the Operation of the Provincial I-Hub and SARAI Hub", activities: [
-        { name: "", months: [1,2,3,4,5,6,7,8,9,10,11,12] },
+      { name: "Provincial I-Hub and SARAI Hub Operations", activities: [
+        { name: "Deployment of KMDR Staff for the Operation of the Provincial I-Hub and SARAI Hub", months: [1,2,3,4,5,6,7,8,9,10,11,12] },
       ]},
     ]
   },
@@ -320,8 +320,8 @@ const data2026 = [
         { name: "3rd Quarter Business Meeting", months: [2] },
         { name: "4th Quarter Business Meeting", months: [5] },
       ]},
-      { name: "Deployment of KMDR Staff for the Operation of the Provincial I-Hub and SARAI Hub", activities: [
-        { name: "", months: [10,11,12] },
+      { name: "Provincial I-Hub and SARAI Hub Operations", activities: [
+        { name: "Deployment of KMDR Staff for the Operation of the Provincial I-Hub and SARAI Hub", months: [10,11,12] },
       ]},
     ]
   },
